2fd3772ace55016400e2a2a52b1480c5ddfbd158
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-04-11 19:26:08 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = {1,2,3,4,5,6,7,8,9};
+        int[] myArraytwo = {10,11,12,13,14,15,16,17,18,19};
+        Console.WriteLine($"Gia tri lon nhat cua mang la {MaxValue(myArray)}");
+Console.WriteLine($"Gia tri lon nhat cua mang 2 la {MaxValue(myArraytwo)}");
+}
+private static int MaxValue (int[] arrayValue)
+{
+    int maxValue=0;
+    for (int i = 0; i<arrayValue.Length; i++)
+    {
+        if (maxValue<arrayValue[i])
+        {
+            maxValue=arrayValue[i];
+        }
+
+    }
+    return maxValue;
+}
+}
